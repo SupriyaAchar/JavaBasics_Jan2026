4f7a8552ad93f211d6f6ae6eff642a7e4d6d01c2
SupriyaK <SupriyaK@ITCRLPT232>
2026-01-12 11:11:00 +0530

    Leap year

--- src/Assignment/LeapYear.java
+++ src/Assignment/LeapYear.java
@@ -5,7 +5,12 @@ public class LeapYear {
 	
 
 	public static void main(String[] args) {
-		
+		int year=2020;
+		if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Common Year");
+        }
 			
 		
 	}
